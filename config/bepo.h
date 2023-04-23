c6fa593ef104d5f69c1843535b147ae2123e0646
Martin.Drillon <martindrillon@gmail.com>
2023-04-23 11:46:40 +0200

    création keymap iteMX

--- config/bepo.h
+++ config/bepo.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <dt-bindings/zmk/keys.h>
 
-#define  A     (A)  // A
-#define  B   (Q)    // B
+// #define  A     (A)  // A
+// #define  B   (Q)    // B
+
+#define A (ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_A))
 
