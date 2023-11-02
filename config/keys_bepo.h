be0e554cf465b7c2ba8df5020fc510224ff74de0
Martin.Drillon <martindrillon@gmail.com>
2023-11-02 09:25:50 +0100

    modification cumbo et test BP_keycode

--- config/keys_bepo.h
+++ config/keys_bepo.h
@@ -1,5 +1,125 @@
 #pragma once
 #include <dt-bindings/zmk/keys.h>
 
-#define DOLLAR (GRAVE)
-#define DLLR (DOLLAR)
+#define BP_DOLLAR (GRAVE)                       // $
+#define BP_DLLR (BP_DOLLAR)                     // $
+
+#define BP_DOUBLE_QUOTES (NUMBER_1)
+#define BP_DQT (BP_DOUBLE_QUOTES)
+
+#define BP_GUILLEMET_GAUCHE (NUMBER_2)
+#define BP_GGA (BP_GUILLEMET_GAUCHE)
+
+#define BP_GUILLEMET_DROIT (NUMBER_3)
+#define BP_GDR (BP_GUILLEMET_DROIT)
+
+#define BP_LEFT_PARENTHESIS (NUMBER_4)
+#define BP_LPAR (BP_LEFT_PARENTHESIS)
+
+#define BP_RIGHT_PARENTHESIS (NUMBER_5)
+#define BP_RPAR (BP_RIGHT_PARENTHESIS)
+
+#define BP_AT_SIGN (NUMBER_6)
+#define BP_AT (BP_AT_SIGN)
+
+#define BP_PLUS (NUMBER_7)
+
+#define BP_MINUS (NUMBER_8) 
+
+#define BP_SLASH (NUMBER_9)
+#define BP_FSLH (BP_SLASH)
+
+#define BP_ASTERISK (NUMBER_0)
+#define BP_ASTRK (ASTERISK)
+#define BP_STAR (BP_ASTERISK)
+
+#define BP_EQUAL (MINUS)
+
+#define BP_PERCENT (EQUAL)
+#define BP_PRCNT (BP_PERCENT)
+
+#define BP_B (Q)
+
+#define BP_EAIGU (W)
+
+#define BP_P (E)
+
+#define BP_O (R)
+
+#define BP_EGRAVE (T)
+#define BP_EGRV
+
+#define BP_CARET (Y)
+
+#define BP_D (U)
+
+#define BP_V (I)
+
+#define BP_L (O)
+
+#define BP_J (P)
+
+#define BP_Z (LEFT_BRACKET)
+
+#define BP_W (RIGHT_BRACKET)
+
+#define BP_A (A)
+
+#define BP_U (S)
+
+#define BP_I (D)
+
+#define BP_E (F)
+
+#define BP_COMMA (G)
+
+#define BP_C (H)
+
+#define BP_T (J)
+
+#define BP_S (K)
+
+#define BP_R (L)
+
+#define BP_N (SEMICOLON)
+
+#define BP_M (SINGLE_QUOTE)
+
+#define BP_CCED (BACKSLASH)
+
+#define BP_ECIR (NON_US_BACKSLASH)
+
+#define BP_AGRAVE (Z)
+#define BP_AGRV (BP_AGRAVE)
+
+#define BP_Y (X)
+
+#define BP_X (C)
+
+#define BP_DOT (V)
+
+#define BP_K (B)
+
+#define BP_SINGLE_QUOTE (N)
+#define BP_SQT (BP_SINGLE_QUOTE)
+#define BP_APOSTROPHE (BP_SQT)
+#define BP_APOS (BP_APOSTROPHE)
+
+#define BP_Q (M)
+
+#define BP_K (B)
+
+#define BP_G (COMMA)
+
+#define BP_H (DOT)
+
+#define BP_F (SLASH)
+
+#define BP_HASH (LS(BP_DLLR))
+
+
+
+
+
+#define BP_UNDERSCORE (RA(SPACE))
+#define BP_UNDER (BP_UNDERSCORE)
